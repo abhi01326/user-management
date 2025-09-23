4cc97945b46328a174804ae43f7d1f6502ce2293
abhi01326 <110410862+abhi01326@users.noreply.github.com>
2025-09-24 01:02:41 +0530

    Update index.js

--- um_backend/index.js
+++ um_backend/index.js
@@ -102,7 +102,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-=======
+
 app.use(express.static(path.join(__dirname, "../um_frontend/dist")));
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../um_frontend/dist/index.html"));
